b84cf3dcc52010c110e03f8b1c9fac179c1405e2
Veronika Nikitina <tomova_veronika@mail.ru>
2021-08-13 15:36:39 +0300

    Исправление замечаний

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {Message} from './Message';
+import { Message } from './components/Message/Message';
 
 const myText = 'Let me introduce myself. My name is Mariya I am a 20-year-old student from Donetsk. I study at the university in my native town and my future profession is bookkeeping. I live with my parents and my elder sister Lena. We are a friendly family. Lena is 2 years older than me. We share our room and tell all our secrets to each other.';
 
@@ -13,5 +13,3 @@ export const App = function (props) {
     </div>
   );
 }
-
-
